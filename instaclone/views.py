1609788d8d63be6fdc7724fd74b6d762343d0b5d
simon <gatheru.simon432@gmail.com>
2019-05-16 15:05:47 +0300

    templates directory and welcome page created

--- instaclone/views.py
+++ instaclone/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def test(request):
-    return render('welcome.html')
+def welcome(request):
+    return render(request,'welcome.html')
+
